ed504db8f01a94bcda0ca27ab7eb141161f3952b
Al Amin <alamin@Als-MacBook-Air.local>
2025-02-13 02:28:38 +0600

    add more string

---
+++ object.js
@@ -0,0 +1,33 @@
+// primitive
+// basic, primary
+const age = 21;
+const school = 'refile square';
+const isPassed = true;
+// let isDeveloper;
+const subject = ['bangla', 'English', 'Physics', 'Math']
+const bottle_wrong = ['white', 45];
+const bottleColor = 'white';
+const bottlePrice = 45;
+const bottleDrinks = 'water';
+
+// console.log(isDeveloper);
+
+// object
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+}
+
+// non-primitive
+const subject = {
+    name: 'biology',
+    teacher: 'rasheda mam',
+    examDate: '30 Feb'
+    chpters: ['first', 'second', 'third'],
+    exam: {
+        name: 'Final Exam',
+        marks: 100
+    }
+}
